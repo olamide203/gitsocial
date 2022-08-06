a3802f57cad33cebf769c91de1461faf50b7acb8
Olamide Atitebi <olamideumarq@gmail.com>
2022-08-06 21:02:51 +0100

    fix:make stat item responsive

--- packages/client/src/components/Dashboard/StatItem.jsx
+++ packages/client/src/components/Dashboard/StatItem.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
 function StatItem({ icon, title, value }) {
-    return (
-        <div className="h-28 grid bg-neutral grid-cols-auto p-3 rounded-2xl shadow-xl hover:bg-gray-800 cursor-pointer">
-            <span className="col-span-2 text-white text-xs sm:text-sm my-0 capitalize">
-                {title}
-            </span>
-            <span className="text-white text-4xl font-bold self-end">
-                {value}
-            </span>
-            <div className="justify-self-end self-end">{icon}</div>
-        </div>
-    );
+  return (
+    <div className="h-28 grid bg-neutral grid-cols-auto p-3 rounded-2xl shadow-xl hover:bg-gray-800 cursor-pointer">
+      <span className="col-span-2 text-white text-xs sm:text-sm my-0 capitalize">
+        {title}
+      </span>
+      <span className="text-white sm:text-4xl font-bold self-end truncate">
+        {new Intl.NumberFormat("en-US", { notation: "compact" }).format(value)}
+      </span>
+      <div className="justify-self-end self-end">{icon}</div>
+    </div>
+  );
 }
 
 export default StatItem;
--- packages/client/src/pages/Dashboard.jsx
+++ packages/client/src/pages/Dashboard.jsx
@@ -17,7 +17,7 @@ function Dashboard() {
   return (
     <div className=" text-white dashboard h-screen w-full overflow-y-scroll">
       {/* <h1 className="text-3xl font-bold">overview</h1> */}
-      <div className="grid grid-cols-auto-fit sm:grid-cols-auto-fit-2 gap-10 p-10 w-full">
+      <div className="grid grid-cols-auto-fit sm:grid-cols-auto-fit-2 gap-10 p-4 sm:p-10 w-full">
         <div className=" grid grid-cols-1 sm:grid-cols-auto gap-x-4 bg-neutral rounded-xl items-center justify-center self-center justify-self-center max-w-[32rem] h-full lg:justify-self-end w-full p-5">
           <div className="avatar online aspect-square mx-auto my-3 max-w-[10rem]">
             <div className=" rounded-full">
@@ -49,7 +49,7 @@ function Dashboard() {
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 items-center justify-center self-center justify-self-center gap-4 max-w-[32rem] h-full lg:justify-self-start w-full">
           <StatItem
             icon={<ImBooks className="text-4xl text-blue-600" />}
-            value={profile.public_repos + profile.owned_private_repos}
+            value={profile.public_repos + profile.total_private_repos}
             title="repos"
           />
           <StatItem
